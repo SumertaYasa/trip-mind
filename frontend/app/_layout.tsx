38d9cf9de3e165e6facdef0fb8aaea28753a5d22
pablooqt <106124645+pablooqt@users.noreply.github.com>
2026-05-10 19:52:26 +0800

    fix: hilangkan default header hitam di halaman plan

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout() {
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(profiling)" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="plan" options={{ headerShown: false }} />
         <Stack.Screen
           name="modal"
           options={{ presentation: "modal", title: "Modal" }}
